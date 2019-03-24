2b025491f92d87960ea47fe8bac28042350bcd4b
mohammed mahmoud <mohammed.95mahmoud@gmail.com>
2019-03-24 16:01:18 +0200

    commit

--- app/src/main/java/com/mahmoud/mohammed/timesapp/presentation/detail/TimesDetailsFragment.java
+++ app/src/main/java/com/mahmoud/mohammed/timesapp/presentation/detail/TimesDetailsFragment.java
@@ -21,8 +21,7 @@ public class TimesDetailsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_times_details, container, false);
     }
